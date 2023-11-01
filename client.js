1511edb7af0136b9cbe46ccd863773c7e1d2691e
Saket Singh <saketaryanite@gmail.com>
2023-11-01 16:22:15 +0530

    async_1

--- client.js
+++ client.js
@@ -1,10 +1,38 @@
 const fetch = require("node-fetch");
 
-async function main() {
-  const response = await fetch("http://localhost:3000/todos?user_id=2");
-  const data = await response.json();
-  console.log(data);
+// Write your code here
+async function fetchUserData() {
+  const response = await fetch("http://localhost:3000/users");
+  const userData = await response.json();
+  return userData;
 }
-main();
 
-// write your code here
+async function main() {
+  const { users } = await fetchUserData();
+
+  const cropCount = 5;
+
+  for (let i = 0; i < users.length; i += cropCount) {
+    const chunk = users.slice(i, i + cropCount);
+
+    const todoPromises = await (chunk.map(async (user) => {
+      const todosResponse = await fetch(`http://localhost:3000/todos?user_id=${user.id}`);
+      const todosData = await todosResponse.json();
+      return { user, todos: todosData };
+    }));
+    const userTodos = await Promise.all(todoPromises)
+
+    // console.log(userTodos);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // console.log(userTodos)
+
+    for (const { user, todos } of userTodos) {
+      const completedTodoCount = todos.todos.filter((todo) => todo.isCompleted == true).length
+      console.log(completedTodoCount);
+    }
+  }
+}
+
+main();
