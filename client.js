a4842503c540d959598e4051fc3c4c084bcb8fc6
Saket Singh <saketaryanite@gmail.com>
2023-11-17 09:43:35 +0530

    updated

--- client.js
+++ client.js
@@ -1,38 +1,48 @@
 const fetch = require("node-fetch");
 
-// Write your code here
-async function fetchUserData() {
-  const response = await fetch("http://localhost:3000/users");
-  const userData = await response.json();
-  return userData;
+let totalTasks = [];
+
+async function fetchData(url) {
+  let data = await fetch(url);
+  data = await data.json();
+  return data;
 }
 
 async function main() {
-  const { users } = await fetchUserData();
+  // Fetch all the users by calling the /users API using node-fetch
+  const getAllUsers = await fetchData("http://localhost:3000/users");
+  console.log(getAllUsers);
 
-  const cropCount = 5;
+  // Fetch todos for each user in chunks of 5 users by calling the /todos API, i.e,
+  // Fetch the todos for first 5 users concurrently.
+  // Once the todos for the first 5 users are fetched, wait for 1 second
+  // and then fetch the todos of the next 5 users concurrently
+  // wait for 1 more second and fetch the todos of the next 5 users concurrently and so on until todos for all users are fetched. We fetch the todos in chunks so as to not overload the server.
 
-  for (let i = 0; i < users.length; i += cropCount) {
-    const chunk = users.slice(i, i + cropCount);
-
-    const todoPromises = await (chunk.map(async (user) => {
-      const todosResponse = await fetch(`http://localhost:3000/todos?user_id=${user.id}`);
-      const todosData = await todosResponse.json();
-      return { user, todos: todosData };
-    }));
-    const userTodos = await Promise.all(todoPromises)
-
-    // console.log(userTodos);
-
-    await new Promise(resolve => setTimeout(resolve, 1000));
-
-    // console.log(userTodos)
-
-    for (const { user, todos } of userTodos) {
-      const completedTodoCount = todos.todos.filter((todo) => todo.isCompleted == true).length
-      console.log(completedTodoCount);
+  for (let i = 1; i < 16; i++) {
+    if (i % 5 === 1 && i !== 1) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
+
+    let response = await fetchData(`http://localhost:3000/todos?user_id=${i}`);
+    console.log(`user ${i}`);
+    console.log(response);
   }
+
+  // Once todos for all users are fetched, calculate how many todos are completed for each user and print the result
+  let todotasks = getAllUsers.users;
+  for (i in todotasks) {
+    let response = await fetchData(`http://localhost:3000/todos?user_id=${i}`);
+    let count = 0;
+    for (j of response.todos) {
+      if (j.isCompleted) {
+        count++;
+      }
+    }
+    todotasks[i].todos = count;
+    totalTasks.push(todotasks[i]);
+  }
+  console.log(totalTasks);
 }
 
 main();
